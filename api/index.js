e7148478d9b9a67ba2ff5310d2b0750ecc36c752
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-05-22 14:56:24 -0500

    Authentication: login

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const swaggerUi = require('swagger-ui-express');
 
 const config = require('../config.js');
 const user = require('./components/user/network');
+const auth = require('./components/auth/network');
 
 const app = express();
 
@@ -10,8 +11,9 @@ app.use(express.json());
 
 const swaggerDoc = require('./swagger.json');
 
-// ROUER
+// ROUTER
 app.use('/api/user', user);
+app.use('/api/auth', auth);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(config.api.port, () => {
