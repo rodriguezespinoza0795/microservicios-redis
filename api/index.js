5d0203498dafb158b4bd9c6772d20d2eb562ed16
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-05-22 20:06:44 -0500

    Advanced error handling

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const swaggerUi = require('swagger-ui-express');
 const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
+const errors = require('../network/errors');
 
 const app = express();
 
 app.use(express.json());
+
 
 const swaggerDoc = require('./swagger.json');
 
@@ -15,6 +17,7 @@ const swaggerDoc = require('./swagger.json');
 app.use('/api/user', user);
 app.use('/api/auth', auth);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use(errors);
 
 app.listen(config.api.port, () => {
     console.log(`La aplicación está escuchando en http://localhost:${config.api.port}`)
