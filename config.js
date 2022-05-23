c3ea310541c0d58b6040d8abbba8ebbb5a1b796a
David Rodriguez <62265080+rodriguezespinoza0795@users.noreply.github.com>
2022-05-22 21:53:04 -0500

    add database

--- config.js
+++ config.js
@@ -5,4 +5,11 @@ module.exports = {
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!',
     },
+    mysql: {
+        host: process.env.MYSQL_HOST || '127.0.0.1',
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASS || '',
+        database: process.env.MYSQL_DB || 'test',
+        port: process.env.MYSQL_PORT || '3308',
+    },
 }
